58333a155fad707867cf35166872d2d13cc79151
fion940410 <fiontemper@yahoo.com.tw>
2017-03-17 16:47:49 +0800

    0317

---
+++ eSale/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSale.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.Custld;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+	}
+}
---
+++ eSale/Models/OrderService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSale.Models
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+                }
+        /// <summary>
+        /// 刪除訂單ById
+        /// </summary>
+        public void DeletOrderById()
+        { 
+        }
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
+        public void UpdateOrder()
+        { 
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id">訂單Id</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.Custld = "GSS";
+            result.CustName = "餓揚資訊";
+            return result; }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrders()
+        { 
+            return new List<Order>(); }
+    }
+}
